de1c4e79109f1edc17334a05c484c744b60a61f8
Leon <mpahmanoah@gmail.com>
2023-09-22 19:01:11 +0200

    footer About responsive

--- burger.js
+++ burger.js
@@ -5,19 +5,17 @@ const TransitionBurger = document.querySelector(".transitionBurger");
 const tl = gsap.timeline();
 
 ContainerBurger.addEventListener("click", () => {
-  tl.to(".transitionBurger", { x: 0 });
+  tl.to(".transitionBurger", {
+    x: 0,
+  });
 });
 
 const ContainerCross = document.querySelector(".ContainerCross");
 const tl1 = gsap.timeline();
 ContainerCross.addEventListener("click", () => {
-  tl1.to(".transitionBurger", { x: innerWidth }).to(".transitionBurger", {
-    opacity: 0,
-    duration: 0.5,
-    onComplete: hideElement,
-  });
+  tl1
+    .set(".transitionBurger", { display: "none" })
+    .to(".transitionBurger", { x: innerWidth });
 
-  function hideElement() {
-    document.querySelector(".transitionBurger").style.display = "none";
-  }
+  function hideElement() {}
 });
